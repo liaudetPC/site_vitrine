b665819075ae87e8e2ed6a37d316f9fc72613286
liaudetPC <108697409+liaudetPC@users.noreply.github.com>
2023-06-30 13:20:12 +0200

    création de la page5
    add ctrl_page5.php
    update nav.php

--- vues/include/nav.php
+++ vues/include/nav.php
@@ -2,5 +2,6 @@
     <li><a href="../ctrl/page1.php">menu 1</a></li>
     <li><a href="../ctrl/page2.php">menu 2</a></li>
     <li><a href="../ctrl/page3.php">menu 3</a></li>
-    <li><a href="../ctrl/page3.php">menu 4</a></li>
+    <li><a href="../ctrl/page4.php">menu 4</a></li>
+    <li><a href="../ctrl/page5.php">menu 5</a></li>
 </ul>
